c01cdb7d3d0c75214123bd930e0575546178ed91
Harini0316 <harinichandrasekar.1603@gmail.com>
2024-08-20 21:53:13 +0530

    Lab004 files added

---
+++ src/ex_20082024/Lab004.java
@@ -0,0 +1,15 @@
+package ex_20082024;
+
+public class Lab004 {
+    public static void main(String[] args) {
+        //defining an 'age' variable
+        int age = 16;
+        //checking the age
+        if (age > 18) {
+            System.out.print("Age is greater than 18");
+        }
+        else{
+            System.out.println("Age is lesser than 18");
+        }
+    }
+}
