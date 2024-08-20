7173e088ce5898ca2c0d6c0ef1a161cfe95d89be
Harini0316 <harinichandrasekar.1603@gmail.com>
2024-08-20 21:04:22 +0530

    This contains the programmings on JAVA

---
+++ src/ex_20082024/Lab001.java
@@ -0,0 +1,11 @@
+package ex_20082024;
+
+public class Lab001 {
+    public static void main(String[] args){
+        System.out.println("Hello  World Harini!!!");
+    }
+}
+
+public static void main(String[] args) {
+
+}
---
+++ src/lab001.java
@@ -0,0 +1,8 @@
+
+
+public class lab001 {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+    }
+}
