286f0233ccc1e04a20efbef7b91d3809d80b02af
Harini0316 <harinichandrasekar.1603@gmail.com>
2024-08-20 21:45:43 +0530

    Readme file added

---
+++ README.md
@@ -0,0 +1,13 @@
+# JAVA PROGRAMS
+
+AUTHOR - HARINI.C 
+This Repo contains the source code for the Exercises that
+we have done in the ATB7x Batch.
+
+-Core JAVA
+-OOPS
+-Collection Framework
+-Multithreading
+-Design Patterns
+-File IO
+-Other Misc Concepts
---
+++ src/ex_20082024/Lab003.java
@@ -0,0 +1,12 @@
+package ex_20082024;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        //defining an 'age' variable
+        int age=20;
+        //checking the age
+        if(age>18) {
+            System.out.print("Age is greater than 18");
+        }
+    }
+}
