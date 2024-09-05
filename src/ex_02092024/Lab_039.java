2d9cda6ba449847792bcdfdc8a9634e70fb6ecfe
Harini0316 <harinichandrasekar.1603@gmail.com>
2024-09-05 22:10:01 +0530

    Added lab 39

--- src/ex_02092024/Lab_039.java
+++ src/ex_02092024/Lab_039.java
@@ -2,24 +2,23 @@ package ex_02092024;
 
 public class Lab_039 {
     public static void main(String[] args) {
-        // ✅ Operators
-        int age = 65;
-        int lambo = -1;
-        // data_type variable_name(identifier) = variable _value(literals)
-        // Unary = Only 1 operand, operator
-        //  age - operand, value - unary plus, = operator
-        //  lambo - operand, value - unary minus, = operator
+        // Arithmetic Operators
+        // +,-,%,/,*
+        // Compound Assignment Operators
+        // +=, -=, /=, *=
+        // Relational Operators
+        // > , < , >= <=, !, != , == (compare two value)
+        // Logical Operators
+        //  OR or and , || , && , !
+        // They will come in the future.  -OOPs concepts
+        // new Operator
+        // instance of Operator
+        // Optional to learn - digital Electronics
+        // Bitwise - Not important for Automation
+        // ~ - Bitwise Not, >> - Right shift, << Left Shift, ^ - XOR
 
+    }
 
-        // Binary Operator
-        int a  = 10;
-        int b = 23;
-        int sum  = a+b;
-        // Binary - Arthemetic +,-,*,/....
-        // Two operand
-        // a, b -> operand , + operator
-        // Ternary Operator a,b,c - operand
 
 
     }
-}
