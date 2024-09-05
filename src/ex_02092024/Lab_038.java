cc81e8d2b311aa1a207a8256b67bec7f7613839a
Harini0316 <harinichandrasekar.1603@gmail.com>
2024-09-05 18:53:29 +0530

    Added lab 38

--- src/ex_02092024/Lab_038.java
+++ src/ex_02092024/Lab_038.java
@@ -1,5 +1,24 @@
 package ex_02092024;
 
 public class Lab_038 {
+    public static void main(String[] args) {
+        // Operators
+        int age = 65;
+        int lambo = -1;
+        // data_type variable_name(identifier) = variable _value(literals)
+        // Unary = Only 1 operand, operator
+        //  age - operand, value - unary plus, = operator
+        //  lambo - operand, value - unary minus, = operator
 
+
+        // Binary Operator
+        int a  = 10;
+        int b = 23;
+        int sum  = a+b;
+        // Binary - Arthe +,-,*,/....
+        // Two operand
+        // a, b -> operand , + operator
+        // Ternary Operator a,b,c - operand
+
+    }
 }
